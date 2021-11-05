651808e505c67bec3e8f694e4e5e1dbda07a40bd
dongchelsea <64898814+vutridonghaui@users.noreply.github.com>
2021-11-05 23:15:32 +0700

    add file

--- src/app/EditorModule/editor.module.ts
+++ src/app/EditorModule/editor.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ArticleDetailComponent } from './article-detail/article-detail.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ArticleDetailComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    ArticleDetailComponent
   ]
 })
 export class EditorModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,41 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './AppModule/footer/footer.component';
-import {AppModuleModule} from "./AppModule/app-module/app-module.module";
-import { TagComponent } from './HomeModule/tag/tag.component';
-import { LoginComponent } from './HomeModule/login/login.component';
-import { SingUpComponent } from './HomeModule/sing-up/sing-up.component';
-import { SettingComponent } from './HomeModule/setting/setting.component';
-import { ArticleDetailComponent } from './EditorModule/article-detail/article-detail.component';
-import { CommentDetailComponent } from './EditorModule/comment-detail/comment-detail.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SignupComponent } from './AuthModule/signup/signup.component';
-import { ArticleListComponent } from './HomeModule/article-list/article-list.component';
-import { ProfileComponent } from './profile/profile.component';
-import { ListArticleComponent } from './profile/list-article/list-article.component';
+// import { TagComponent } from './HomeModule/tag/tag.component';
+// import { LoginComponent } from './AuthModule/login/login.component';
+// import { SettingComponent } from './AuthModule/setting/setting.component';
+// import { ArticleDetailComponent } from './EditorModule/article-detail/article-detail.component';
+// import { CommentDetailComponent } from './EditorModule/comment-detail/comment-detail.component';
+// import { HeaderComponent } from './shared/header/header.component';
+// import { FooterComponent } from './shared/footer/footer.component';
+// import { SignupComponent } from './AuthModule/signup/signup.component';
+// import { ArticleListComponent } from './HomeModule/article-list/article-list.component';
+// import { ProfileComponent } from './ProfileModule/profile.component';
+// import { ListArticleComponent } from './ProfileModule/list-article/list-article.component';
+import { EditorModule } from './EditorModule/editor.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TagComponent,
-    LoginComponent,
-    SingUpComponent,
-    SettingComponent,
-    ArticleDetailComponent,
-    CommentDetailComponent,
-    HeaderComponent,
-    FooterComponent,
-    SignupComponent,
-    ArticleListComponent,
-    ProfileComponent,
-    ListArticleComponent,
+    // TagComponent,
+    // LoginComponent,
+    // SignupComponent,
+    // SettingComponent,
+    // ArticleDetailComponent,
+    // CommentDetailComponent,
+    // HeaderComponent,
+    // FooterComponent,
+    // SignupComponent,
+    // ArticleListComponent,
+    // ProfileComponent,
+    // ListArticleComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AppModuleModule
+    EditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
